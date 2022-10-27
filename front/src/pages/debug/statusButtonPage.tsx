5a24f4896c33da6ee9cf3cdd6d917765990cc0ff
rf-castle <kouta.tashiro.2000@gmail.com>
2022-10-27 23:01:02 +0900

    debugページ追加

---
+++ front/src/pages/debug/statusButtonPage.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import StatusButtons from '../../components/statusButtons';
+
+const testPage = () => {
+  return (
+      <StatusButtons/>
+  )
+}
+
+export default testPage
