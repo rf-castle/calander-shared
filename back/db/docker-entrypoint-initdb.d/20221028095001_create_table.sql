832191704c67a32e53b8cc9ddc4671cdcbf11713
soukouki <soukouki0@yahoo.co.jp>
2022-10-29 01:33:01 +0900

    db定義の追加

---
+++ back/db/docker-entrypoint-initdb.d/20221028095001_create_table.sql
@@ -0,0 +1,53 @@
+
+-- +goose Up
+
+CREATE DATABASE db;
+
+USE db;
+
+CREATE TABLE Users (
+  user_id VARCHAR(255) PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE OriginalCalenders (
+  calender_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  user_id VARCHAR(255) NOT NULL
+    REFERENCES Users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  webcal_url VARCHAR(1023) NOT NULL
+);
+
+CREATE TABLE Filters (
+  filter_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  member_id INT UNSIGNED NOT NULL
+    REFERENCES Members(member_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  filter_order INT UNSIGNED NOT NULL,
+  filter_query VARCHAR(255) NOT NULL,
+  filtering_calender_id INT UNSIGNED
+    REFERENCES OriginalCalenders(calender_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  filterd_publicity ENUM('open', 'shadow') NOT NULL,
+  filterd_availability ENUM('ok', 'so-so', 'ng', 'ignore') NOT NULL,
+  UNIQUE (member_id, filter_order)
+);
+
+CREATE TABLE Members (
+  member_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  user_id VARCHAR(255) NOT NULL
+    REFERENCES Users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  room_id VARCHAR(255) NOT NULL
+    REFERENCES Rooms(room_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  nickname VARCHAR(255) NOT NULL,
+  UNIQUE (user_id, room_id)
+);
+
+CREATE TABLE Rooms (
+  room_id VARCHAR(255) PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Webhooks (
+  room_id VARCHAR(255) NOT NULL
+    REFERENCES Rooms(room_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  service_name VARCHAR(255) NOT NULL,
+  url VARCHAR(1023) NOT NULL
+);
