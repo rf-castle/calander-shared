1a2c2ca5a968629b383b597f26460df6cb4e1bc0
rf-castle <kouta.tashiro.2000@gmail.com>
2022-10-29 13:37:15 +0900

    usecaseを追加した

--- back/src/usecase/usecase.go
+++ back/src/usecase/usecase.go
@@ -2,6 +2,15 @@ package usecase
 
 import "github.com/rf-castle/calander-shared/back/src/domain"
 
+type RoomInformation struct {
+	Room    *domain.Room
+	members []*domain.Member
+}
+
 type UseCase interface {
 	CreateUser(name string) (*domain.User, error)
+	GetJoinedRooms(userId string) ([]*domain.RoomResult, error)
+	CreateRoom(userId string, name string) (*domain.Room, error)
+	ChangeMemberRole(roomId string, userId string, userRole string) error
+	GetRoomById(userId string) (*RoomInformation, error)
 }
