10ad84eb70d92f8dfd19f18c5d609d40a8e68b44
soukouki <soukouki0@yahoo.co.jp>
2022-10-30 11:08:26 +0900

    UseCaseに空の実装を追加

--- back/src/usecase/usecase.go
+++ back/src/usecase/usecase.go
@@ -21,3 +21,59 @@ type UseCase interface {
 		filterdAvailability domain.Availability) error
 	SwapFilter(filterAId uint32, filterBId uint32) error
 }
+
+type UseCaseImpl struct {
+	userRepository domain.UserRepository
+	roomRepository domain.RoomRepository
+	memberRepository domain.MemberRepository
+	filterRepository domain.FilterRepository
+	calendarRepository domain.CalendarRepository
+}
+
+func NewUseCase(
+	userRepository domain.UserRepository,
+	roomRepository domain.RoomRepository,
+	memberRepository domain.MemberRepository,
+	filterRepository domain.FilterRepository,
+	calendarRepository domain.CalendarRepository) UseCase {
+	return &UseCaseImpl{
+		userRepository: userRepository,
+		roomRepository: roomRepository,
+		memberRepository: memberRepository,
+		filterRepository: filterRepository,
+		calendarRepository: calendarRepository,
+	}
+}
+
+func (this *UseCaseImpl) CreateUser(name string) (*domain.User, error) {
+	panic("not implemented")
+}
+
+func (this *UseCaseImpl) GetJoinedRooms(userId string) ([]RoomInformation, error) {
+	panic("not implemented")
+}
+
+func (this *UseCaseImpl) CreateRoom(userId string, name string) (*domain.Room, error) {
+	panic("not implemented")
+}
+
+func (this *UseCaseImpl) ChangeMemberRole(roomId string, userId string, userRole string) error {
+	panic("not implemented")
+}
+
+func (this *UseCaseImpl) GetRoomById(userId string) (*RoomInformation, error) {
+	panic("not implemented")
+}
+
+func (this *UseCaseImpl) AddFilter(
+	userId string,
+	filterQuery string, 
+	filteringCalendarId *uint32, 
+	filterdPublicity domain.Publicity, 
+	filterdAvailability domain.Availability) error {
+	panic("not implemented")
+}
+
+func (this *UseCaseImpl) SwapFilter(filterAId uint32, filterBId uint32) error {
+	panic("not implemented")
+}
