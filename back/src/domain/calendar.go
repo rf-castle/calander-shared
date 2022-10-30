bc57a509bdffc208460839dace704441784d4ffa
Kayi <gpioblink@gmail.com>
2022-10-30 15:34:42 +0900

    reconstruct calender

--- back/src/domain/calendar.go
+++ back/src/domain/calendar.go
@@ -3,12 +3,12 @@ package domain
 type Calendar struct {
 	CalendarId     *uint32
 	CalenderName   string
-	CalenderUserId string
+	CalenderUserId *string
 	// TODO: add more fields
 	Events []*Event
 }
 
 type CalendarRepository interface {
-	Save(calendar *Calendar) (*Calendar, error) // repoで登録するときはCalenderId=nilは許さない
+	Save(calendar *Calendar) (*Calendar, error) // repoで登録するときはCalenderId,CalenderUserIdがnilは許さない
 	Get(calendarId uint32) (*Calendar, error)
 }
