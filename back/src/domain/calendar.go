e9773316134d71be2ca574d531f28d9748276463
Kayi <gpioblink@gmail.com>
2022-10-30 14:58:57 +0900

    remove bad fields

--- back/src/domain/calendar.go
+++ back/src/domain/calendar.go
@@ -2,20 +2,11 @@ package domain
 
 type Calendar struct {
 	CalendarId     uint32
-	CalenderId     uint32
 	CalenderName   string
 	CalenderUserId string
-	TimeZone       TimeZone
 	// TODO: add more fields
-	// sou7はCalendarがUserIdを持つのはおかしいと思っているけれど、他の人はどう考えるか話したいなと
 	Events []*Event
 }
-
-type TimeZone string
-
-const (
-	TOKYO = Role("Japan/Tokyo")
-)
 
 type CalendarRepository interface {
 	Save(calendar *Calendar) (*Calendar, error)
