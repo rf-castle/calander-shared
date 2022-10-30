89e4a454dd1cea947242b4d8aea86f68f029a590
soukouki <soukouki0@yahoo.co.jp>
2022-10-30 11:56:25 +0900

    よく見たらgoがローカルに入っていたのでフォーマット用のスクリプトいらなかった

--- back/scripts/gofmt.sh
+++
@@ -1,6 +0,0 @@
-cd $(cd $(dirname $0)/../; pwd)
-CONTAINER=$(docker-compose run --no-deps -d --rm web bash)
-docker cp $PWD/src $CONTAINER:/go/src/app/src
-docker exec -it ${CONTAINER} bash -c "go fmt ./src/..."
-docker cp $CONTAINER:/go/src/app/src/. $PWD/src
-docker rm -f ${CONTAINER}
