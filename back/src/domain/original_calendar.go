188c4edc271664f2acd00c60c97e2c34b70c4114
soukouki <soukouki0@yahoo.co.jp>
2022-10-30 15:02:25 +0900

    OriginalCalendarの形式を決定

---
+++ back/src/domain/original_calendar.go
@@ -0,0 +1,18 @@
+package domain
+
+type OriginalCalendar struct {
+	OriginalCalendarId uint32
+	Name string
+	UserId string
+	CalendarId uint32
+	FetchOrigin FetchOriginModel
+}
+
+type FetchOriginModel interface {
+	Fetch() (Calendar, error)
+}
+
+type OriginalCalendarRepository interface {
+	Save(originalCalendar OriginalCalendar)
+	Get(originalCalendarId uint32) (OriginalCalendar, error)
+}
